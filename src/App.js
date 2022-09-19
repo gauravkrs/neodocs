4ce59e1eff975e87a9455ce1ebc6e77f83772aca
Gaurav Kumar <gauravkr2179@gmail.com>
2022-09-19 10:39:07 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Nested } from "./components/Nested";
+import { data } from "./components/data";
+import { useState } from "react";
+import { Display } from "./components/Display";
 
 function App() {
-  
+  const [info, setInfo] = useState(false);
   return (
     <div className="App">
-      <Nested />
+      <div>
+        <h2>Nested Check Box Of Teacher & Children</h2>
+        <div className="grid-container">
+          {data?.map((el, index) => (
+            <Nested key={index} store={el} data={info} setData={setInfo} />
+          ))}
+        </div>
+      </div>
+      <div>
+        <h2>Selected Techer and Student Data</h2>
+        <div className="grid-container">
+          {data.map((el, index) => (
+            <Display key={index} info={el} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/data.js
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    teacher: "Teacher1",
+    checked: false,
+    children: [
+      { value: "Student1" },
+      { value: "Student2" },
+      { value: "Student3" },
+      { value: "Student4" },
+    ],
+  },
+  {
+    teacher: "Teacher2",
+    checked: false,
+    children: [
+      { value: "Student1" },
+      { value: "Student2" },
+      { value: "Student3" },
+      { value: "Student4" },
+    ],
+  },
+  {
+    teacher: "Teacher3",
+    checked: false,
+    children: [
+      { value: "Student1" },
+      { value: "Student2" },
+      { value: "Student3" },
+      { value: "Student4" },
+    ],
+  },
+  {
+    teacher: "Teacher4",
+    checked: false,
+    children: [
+      { value: "Student1" },
+      { value: "Student2" },
+      { value: "Student3" },
+      { value: "Student4" },
+    ],
+  },
+  {
+    teacher: "Teacher5",
+    checked: false,
+    children: [
+      { value: "Student1" },
+      { value: "Student2" },
+      { value: "Student3" },
+      { value: "Student4" },
+    ],
+  },
+];
